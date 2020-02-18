894b2fb08265626eabfb0124041e14a140ceef04
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-18 22:29:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ class Tree{
 
 class Deciduous  extends Tree{
   constructor(name){
+    
+    super(species);
     this.name=name;
-    super(species)
+  }
+  
+  static definition(){
+    
   }
 }
